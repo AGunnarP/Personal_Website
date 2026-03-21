47bdc9cc8b3be79db8c3b5ace3d3cda50082404b
Anthony Page <AnthonyGunnarPage@gmail.com>
2026-03-20 21:30:28 -0700

    Tweaked some text

--- src/frontend/Content_Pages/Tutoring.tsx
+++ src/frontend/Content_Pages/Tutoring.tsx
@@ -10,7 +10,7 @@ function Blurb({score}: {score : number}){
         <div>
 
             <p>
-                I am currently a tutor teaching Math and Data Structures and algorithms. I have six months of education experience and have carried out over 100 tutoring sessions. (I hate to brag about this but I feel the need to prove my credibility as a DSA tutor.) I am also within the top <a href="https://leetcode.com/u/AnthonyPage">{score}%</a> of users on Leetcode based on elo. I charge a rate at $45/hr.
+                I am currently a tutor teaching Math and Data Structures and Algorithms (DSA). I have six months of education experience and have carried out over 100 tutoring sessions. (I hate to brag about this but I feel the need to prove my credibility as a DSA tutor.) I am also within the top <a href="https://leetcode.com/u/AnthonyPage">{score}%</a> of users on Leetcode based on elo. I charge a rate at $45/hr.
             </p>
 
         </div>
